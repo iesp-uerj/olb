0848d5146a676989b0872bb4cd7a03eabd0dacf1
Fernando M <meirelesff@hotmail.com>
2019-03-20 16:01:44 -0300

    New function

--- R/gera_relatorio.R
+++ R/gera_relatorio.R
@@ -5,21 +5,23 @@
 #'
 #' @param ini Starting date in the YYYY-MM-DD format (\code{character}).
 #' @param end Ending date in the YYYY-MM-DD format (\code{character}).
+#' @param titulo Title of the report.
+#' @param autor Author of the report (defaults to OLB).
+#' @param out_dir Output directory to store the report.
 #'
 #' @return A report and its corresponding .Rmd file.
 #'
 #' @export
 
-gera_relatorio <- function(ini, end) {
+gera_relatorio <- function(ini, end, titulo, autor, out_dir = getwd()) {
+
   rmarkdown::render(
     system.file("reports", "report_html.Rmd", package = "olb"),
     envir = new.env(),
     encoding = "UTF-8",
-    params = list(
-      ini = ini,
-      end = end
-    ),
-    output_file = paste0("Relatorio -", ini, " - ", end, ".html")
+    params = list(ini = ini, end = end, set_author = autor, set_title = titulo),
+    output_dir = out_dir,
+    output_file = paste0("relatorio_", ini, "_", end, ".html")
   )
 }
 
