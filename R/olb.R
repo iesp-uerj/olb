950e90ae08cdcedc042abe6a4fd866f52d12a782
Fernando M <meirelesff@hotmail.com>
2019-04-03 10:54:44 -0300

    New function

--- R/olb.R
+++ R/olb.R
@@ -21,3 +21,53 @@ get_track_report_data <- function(dataInicio, dataFim, codTema){
     dplyr::mutate(relacionadas = purrr::map(.$id, get_related_proposals))
 }
 
+
+#' Collect data to update the OLB's legislative monitor
+#'
+#' @description
+#' \code{get_monitor_data()} gathers a dataset with all the information required to produce
+#' OLB's llegislative monitor.
+#'
+#' @param dataInicio Start date (in YYYY-MM-DD format, as \code{character}).
+#' @param dataFim End date (in YYYY-MM-DD format, as \code{character}).
+#' @param codTema Topic code (use \code{\link{list_topics}} to obtain a list of all valid topic codes).
+#'
+#' @return A \code{tibble} with some \code{nested} columns.
+#'
+#' @importFrom rlang .data
+#'
+#' @export
+
+get_monitor_data <- function(dataInicio, dataFim, codTema){
+
+  # Util
+  is_zero <- function(x) length(x) == 0
+
+  # Longitudinal data
+  long <- get_proposals_olb(dataApresentacaoInicio = "1998-01-01", dataApresentacaoFim = dataFim, codTema = codTema) %>%
+    dplyr::select(.data$id, .data$siglaTipo, .data$codTipo, .data$numero, .data$ano, .data$ementa)
+
+  # Main data
+  props <- get_proposals_olb(dataInicio = dataInicio, dataFim = dataFim, codTema = codTema) %>%
+    dplyr::select(.data$id, .data$siglaTipo, .data$codTipo, .data$numero, .data$ano, .data$ementa) %>%
+    dplyr::mutate(detalhes = purrr::map(.$id, get_proposal)) %>%
+    dplyr::mutate(inteiroteor = purrr::map(.$detalhes, ~ .$urlInteiroTeor) %>% as.character) %>%
+    dplyr::select(-.data$detalhes)
+
+  # Track
+  trams <- props %>%
+    dplyr::mutate(tramitacao = purrr::map(.$id, get_proposal_history)) %>%
+    tidyr::unnest(.data$tramitacao)
+
+  # Related
+  relac <- props %>%
+    dplyr::mutate(relacionadas = purrr::map(.$id, get_related_proposals)) %>%
+    dplyr::mutate(relacionadas = purrr::map_if(.$relacionadas, is_zero, data.frame)) %>%
+    tidyr::unnest(.sep = "_")
+
+  # Authors
+
+
+  # Return
+  list(long = long, props = props, trams = trams, relac = relac)
+}
