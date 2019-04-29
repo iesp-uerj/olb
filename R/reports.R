1170ef1ebeae2e845f78135004ca6b22251cbb22
meirelesff <meirelesff@users.noreply.github.com>
2019-04-29 17:10:30 -0300

    Update olb

--- R/reports.R
+++ R/reports.R
@@ -1,32 +1,25 @@
-#' Produce a report of legislative activity in a given period
+#' Build the OLB monitor
 #'
 #' @description
 #' \code{gera_relatorio()} summarizes legislative data and outputs a Rmarkdown report.
 #'
 #' @param ini Starting date in the YYYY-MM-DD format (\code{character}).
 #' @param end Ending date in the YYYY-MM-DD format (\code{character}).
-#' @param titulo Title of the report.
-#' @param autor Author of the report (defaults to 'OLB').
-#' @param tema Theme of the report (defaults to 'Economia').
 #' @param out_dir Output directory to store the report.
 #'
 #' @return A report and its corresponding .Rmd file.
 #'
+#' @export
 
-# Nao exportada
-gera_relatorio <- function(ini, end, titulo = "Relatorio de Acompanhamento",
-                           autor = "OLB", tema = "Economia", out_dir = getwd()) {
+build_monitor <- function(ini, end, out_dir = getwd()) {
+
 
   rmarkdown::render(
-    system.file("reports", "report_html.Rmd", package = "olb"),
+    system.file("reports", "monitor_olb.Rmd", package = "olb"),
     envir = new.env(),
     encoding = "UTF-8",
-    params = list(ini = ini, end = end, set_author = autor, set_title = titulo, tema = tema),
+    params = list(ini = ini, end = end),
     output_dir = out_dir,
-    output_file = paste0("relatorio_", ini, "_", end, ".html")
+    output_file = "index.html"
   )
 }
-
-
-
-
